ae15f8b06b232a9075584d393d0225bf26c551bd
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-04 08:56:14 -0300

    exercicios de condicionais simples em JavaScript

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -7,6 +7,45 @@
 </head>
 <body>
     <h1>Trabalhando com estruturas condicionais</h1>
-    
+    <hr>
+
+//Lembrete: ao usar condicionais, muitas vezes também usaremos operadores relacionais:
+//<, <=, >, >=, ==, !=, ===, !==
+    <h2>Condicional Simples: <code>if</code></h2>
+    <?php 
+    $numero = 50;
+
+    //Estrutura tradicional(comando , parênteses, chaves)
+    if($numero > 10){
+        echo "<p>$numero é maior que 10.</p>";
+    }
+
+    //Estrutura abreviada (sem chaves)
+    if($numero > 10) echo "<p>$numero é maior que 10.</p>";
+
+    //Estrutura alternativa (sem chaves, com operadores)
+    if($numero > 10):
+        echo "<p>$numero é maior que 10.</p>";
+    endif;
+    ?>
+    <hr>
+    <h2>Condicional Composta: <code>if/else</code></h2>
+    <?php 
+    $produto = "ultrabook";
+    $qtdEmEstoque = 30;
+    $qtdCritica = 5;
+?>
+
+
+<h3><?= $produto ?> ?></h3>
+<p><b>Quantidade em estoque:</b><?= $qtdEmEstoque ?></p>
+<?php 
+if($qtdEmEstoque < $qtdCritica){
+    echo "<p>É necessário comprar/repor</p>";
+} else{
+    echo "<p> Estoque normal.</p>";
+}
+
+?>
 </body>
 </html>
