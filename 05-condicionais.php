59776364ac10fb1a02d97db783602efd526bdd70
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-04 09:37:51 -0300

    sistema de controle de estoque condicional aninhada

--- 05-condicionais.php
+++ 05-condicionais.php
@@ -1,55 +1,69 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP - condicionais</title>
     <style>
-        .comprar {color: red;}
-        .normal { color: blue;}
+        .comprar {
+            color: red;
+        }
+
+        .normal {
+            color: blue;
+        }
     </style>
 </head>
+
 <body>
     <h1>Trabalhando com estruturas condicionais</h1>
     <hr>
 
-//Lembrete: ao usar condicionais, muitas vezes também usaremos operadores relacionais:
-//<, <=, >, >=, ==, !=, ===, !==
-    <h2>Condicional Simples: <code>if</code></h2>
-    <?php 
-    $numero = 50;
+    <!-- Lembrete: ao usar condicionais, muitas vezes também usaremos operadores relacionais: -->
+    <!-- <, <=,>, >=, ==, !=, ===, !== -->
+        <h2>Condicional Simples: <code>if</code></h2>
+        
+        <?php
+        // 
+        $numero = 50;
 
-    //Estrutura tradicional(comando , parênteses, chaves)
-    if($numero > 10){
-        echo "<p>$numero é maior que 10.</p>";
-    }
+        //Estrutura tradicional(comando , parênteses, chaves)
+        if ($numero > 10) {
+            echo "<p>$numero é maior que 10.</p>";
+        }
 
-    //Estrutura abreviada (sem chaves)
-    if($numero > 10) echo "<p>$numero é maior que 10.</p>";
+        //Estrutura abreviada (sem chaves)
+        if ($numero > 10) echo "<p>$numero é maior que 10.</p>";
 
-    //Estrutura alternativa (sem chaves, com operadores)
-    if($numero > 10):
-        echo "<p>$numero é maior que 10.</p>";
-    endif;
-    ?>
-    <hr>
-    <h2>Condicional Composta: <code>if/else</code></h2>
-    <?php 
-    $produto = "ultrabook";
-    $qtdEmEstoque = 03;
-    $qtdCritica = 5;
-?>
+        //Estrutura alternativa (sem chaves, com operadores)
+        if ($numero > 10):
+            echo "<p>$numero é maior que 10.</p>";
+        endif;
+        ?>
+        <hr>
+        <h2>Condicional Composta: <code>if/else</code></h2>
+        <?php
+        $produto = "ultrabook";
+        $qtdEmEstoque = 03;
+        $qtdCritica = 5;
+        ?>
 
 
-<h3><?= $produto ?> ?></h3>
-<p><b>Quantidade em estoque:</b><?= $qtdEmEstoque ?></p>
-<?php 
-if($qtdEmEstoque < $qtdCritica){
-    echo "<p class=\"comprar\">É necessário comprar/repor</p>";
-} else{
-    echo "<p class =\"normal\"> Estoque normal.</p>";
-}
+        <h3><?= $produto ?></h3>
+        <p><b>Quantidade em estoque:</b><?= $qtdEmEstoque ?></p>
+        <?php
+        if ($qtdEmEstoque < $qtdCritica) {
+            echo "<p class=\"comprar\">É necessário comprar/repor</p>";
+            //Condicional aninhada (ela esta dentro de outro if)
+            if ($qtdEmEstoque === 0) {
+                echo "<p><mark class=\"comprar\"></mark></p>";
+            }
+        } else {
+            echo "<p class =\"normal\"> Estoque normal.</p>";
+        }
 
-?>
+        ?>
 </body>
+
 </html>
