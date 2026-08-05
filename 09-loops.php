69a1715b8072f3028976cd1eebb3814222cb985b
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-05 11:35:47 -0300

    Trabalhando com comandos de repetição

---
+++ 09-loops.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loops no PHP</title>
+</head>
+<body>
+    <h1>Trabalhando com comandos de repetição</h1>
+    <hr>
+
+    <h2>While (enquanto)</h2>
+    <p>Executa ações repetidas vezes <b>enquanto a condição for <b>verdadeira</b>.</b></p>
+    
+    <?php 
+    $i = 1;
+    while($i <= 5): //ou vc usa {
+    ?>    
+    <p>Parágrafo: <?= $i ?></p>
+    <?php 
+    $i++;
+ endwhile; //ou }
+    ?>
+
+
+
+    
+
+    
+</body>
+</html>
