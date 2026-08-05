497586b6fbf1143bbefb1f3217553b13385972a3
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-05 11:55:24 -0300

    for(para) Executa ações de acordo com uma quantidade determinada vezes

--- 09-loops.php
+++ 09-loops.php
@@ -21,7 +21,40 @@
     $i++;
  endwhile; //ou }
     ?>
+ <hr>
 
+ <h2>do/While (Faça/enquanto)</h2>
+ <p>Executa ações pelo menos <b>uma vez</b> e, se a condição for veradeira, continua executando outras vezes.</p>
+
+ <?php 
+ $j = 1;
+ do{
+?>
+<div>
+<h3>Título qualquer...</h3>
+<p>Bloco <?= $j ?></p>
+</div>
+
+<?php
+ $j++;
+} while($j <= 3)
+ 
+ ?>
+ <hr>
+
+ <h2>for(para)</h2>
+ <p>Executa ações de acordo com uma <b>quantidade determinada vezes</b>.</p>
+
+ 
+ <section>
+ <h3>Conteúdo da seção</h3>
+ <?php for($i = 1; $i <= 5; $i++){ ?>
+ <details>
+    <summary>Pergunta <?= $i ?></summary>
+    <p>Resposta <?= $i ?></p>
+ </details>
+ <?php }// ou endfor; ?>
+</section>
 
 
     
