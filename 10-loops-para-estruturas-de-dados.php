db948f10bb4962c7c4013a7f59288a61541f7069
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-10 09:59:35 -0300

    usei loops for com Arrays

--- 10-loops-para-estruturas-de-dados.php
+++ 10-loops-para-estruturas-de-dados.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,15 +11,31 @@
 
 
 </head>
+
 <body>
-     <div class="conatiner">
-    <h1>Loops para estruturas de dados</h1>
-    <hr>
-     </div>
+    <div class="conatiner">
+        <h1>Loops para estruturas de dados</h1>
+        <hr>
+        <?php
+        $meses = ["janeiro", "fevereiro", "março", "Abril", "maio", "junho"];
+
+        ?>
+
+        <h2>Usando o loop for para acessar o Array</h2>
+
+        <ol>
+            <?php for($i = 0; $i < count($meses); $i++){?>
+             <li><?= $meses[$i] ?></li>
+       <?php } ?>
+       </ol>
+
+
+    </div>
 
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
