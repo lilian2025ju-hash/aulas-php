e6fa32ae70952c9791ff763d0e458b8c7147b0f5
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-10 09:24:19 -0300

    aplicando bootstrep aos exemplos

--- 08-intercalando-php-e-html.php
+++ 08-intercalando-php-e-html.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Revisando PHP Intercalado com HTML</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
 <body>
     <h1>Revisando PHP Intercalado com HTML</h1>
@@ -59,6 +62,8 @@ endif;
     }
     ?>
     <p><?= $htmlSaida ?> é <?= $palavra ?> de idade</p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
     
 </body>
