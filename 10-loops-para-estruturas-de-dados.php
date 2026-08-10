a539616d2bc975ff60bfc3266935adee1ad34772
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-10 11:17:09 -0300

    Usando foreach para Array associativo

--- 10-loops-para-estruturas-de-dados.php
+++ 10-loops-para-estruturas-de-dados.php
@@ -24,10 +24,96 @@
         <h2>Usando o loop for para acessar o Array</h2>
 
         <ol>
-            <?php for($i = 0; $i < count($meses); $i++){?>
-             <li><?= $meses[$i] ?></li>
-       <?php } ?>
-       </ol>
+            <?php for ($i = 0; $i < count($meses); $i++) { ?>
+                <li><?= $meses[$i] ?></li>
+            <?php } ?>
+        </ol>
+        <hr>
+
+        <h2>Usando o loop for para acessar uma matrix (array de arrays)</h2>
+        <?php
+        $planosDeEstudos = [
+            ["JS Avançado", "Node.js", "Nest.js"],
+            ["PHP", "Orientação a Objetos"],
+            ["Teorias das Cores", "photoshop com IA", "Ux/UI"]
+        ];
+
+        $linhas = count($planosDeEstudos);
+        for ($i = 0; $i < $linhas; $i++): // acessa cada linha
+            $colunas = count($planosDeEstudos[$i]);
+            for ($j = 0; $j < $colunas; $j++): //acessa cada coluna
+
+        ?>
+                <p><?php $planosDeEstudos[$i][$j] ?></p>
+
+        <?php
+            endfor; //fim do acesso a cada coluna
+        endfor; //fimm de acesso a cada linha
+
+        ?>
+
+        <hr>
+
+        <h2>Usando o Loop para cada arrays</h2>
+
+        <?php 
+        $alunos =[ "thiago", "Adela", "Renan", "Pérola"];
+        foreach($alunos as $aluno):
+        ?>
+
+        <p><?= $aluno ?></p>
+        <?php 
+        endforeach;
+        ?>
+
+        <hr>
+        <h2>Usando foreach para Array associativo</h2>
+
+    <?php 
+    $curso = [
+      "titulo" => "Gastronomia",
+      "carga-horaria" => 200,
+      "descricao" => "Aprender o básico sobre a área"
+ ];
+
+ //Extraindo chave com valor
+ foreach($curso as $chave => $valor):
+?>
+  <p><b><?= $chave ?></b>: <?= $valor ?></p>
+
+  <?php 
+  endforeach;
+
+  //Extraindo somente o valor
+  foreach($curso as $valor):
+  ?>
+  <p><i><?= $valor ?></i></p>
+  <?php 
+  endforeach;
+  ?>
+  <hr>
+
+  <h2>Usando foreach em uma matriz</h2>
+
+  <?php 
+  $clientes = [
+    [  "nome" => "juliene",
+      "email" => "ju@senac.br",
+  ],
+  [
+    "nome" => "luiz",
+    "email" =>"luiz@senac.br"
+
+  ]
+
+  ];
+
+  foreach($clientes as $clientes):
+
+  ?>
+  <p><b>nome:</b><span class=""></span></p>
+
+
 
 
     </div>
