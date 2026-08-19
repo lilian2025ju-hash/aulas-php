766f159e26507f2c26026f0d3087c641cafb847a
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-19 11:38:18 -0300

    separa cabecalho e rodape

---
+++ site/includes/cabecalho.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usando PHP</title>
+</head>
+<body>
+    <header>
+        <h1>Site com PHP</h1>
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="cursos.php">Cursos</a>
+            <a href="duvidas.php">Dúvidas</a>
+        </nav>
+    </header>
+    
+
+    <main>
---
+++ site/index.php
@@ -0,0 +1,7 @@
+<?php require "includes/cabecalho.php"  ?>
+
+        <h2>Bem-vindo ao site de Exemplo</h2>
+        <p>Esta é a primeira página do nosso site</p>
+
+<?php require "includes/rodape.php"  ?>
+    
