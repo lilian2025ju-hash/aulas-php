cc754e5a65dec334dd6eebe623e47f1524ad4050
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-19 11:14:27 -0300

    Inclusão de Recursos-

--- 16-inclusao-de-recursos.php
+++ 16-inclusao-de-recursos.php
@@ -1,4 +1,4 @@
-<?php include "recursos.php"; ?>
+<?php require "recursos.php"; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,14 +10,27 @@
     <div class="container">
     <h1>Inclusão de Recursos</h1>
     <hr>
-    <p>Utilizamos os comandos <code>include</code> e/ou <code>require</code>para importar arquivos com recursos externos de qualquer natureza, permitindo assim a reutilizaçao de código</p>
+    <p>Utilizamos os comandos <code>include</code> e/ou <code>require</code> para importar arquivos com recursos externos de qualquer natureza, permitindo assim a reutilizaçao de código</p>
      
 
     <h2>Exemplos de uso/acesso</h2>
-    <p>Estamos estudando no <?= Escola ?> fazendo o curso <?= $curso ?>.</p>
+    <p>Estamos estudando no <?= Escola ?> fazendo o curso <?= $curso ?></p>
     <p>Para fazer este curso o aluno deve ser maior idade.</p>
     <p>Como você <?= Aluno ?> tem 20 anos, você é <?= verificaIdade(20) ?></p>
 
+    <h2>Exemplo de carregamento de conteúdo parcial/fragmento</h2>
+    <section>
+        <h3>título qualquer</h3>
+        <?php include "textos.php"; ?>
+    </section>
+
+    <ul>
+        <?php foreach ($tecnologias as $tecnologia): ?>
+            <li><?= $tecnologia ?></li>
+            <?php endforeach; ?>
+    </ul>
+
+    
 </div>
     
    
--- textos.php
+++ textos.php
@@ -1,5 +1,7 @@
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil eius nobis excepturi reiciendis incidunt rerum quaerat error provident quo velit accusantium illum vitae voluptates voluptate, quidem molestiae qui facere neque!</p>
 
+<?php  include "lista.html" ?>
+
 
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, fugiat! Distinctio molestias modi dolorum .</p>
 
