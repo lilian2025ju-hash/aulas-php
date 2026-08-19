cf935dfadfa7a9b98b9cc414dd604104516e77c3
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-19 12:02:13 -0300

    includes/cabecalho-e--rodape

---
+++ site/includes/duvidas.php
@@ -0,0 +1,19 @@
+<?php require "includes/cabecalho.php"  ?>
+
+        <h2>Página de cursos</h2>
+        <p>Veja ae resposta frequentes:</p>
+        <details>
+            <summary>Quem será o próximo reprovado?
+                <p>Perola!!</p>
+                <details>
+                    <summary>Quem vai pagar umas paçocas?</summary>
+                    <p>Anderson</p>
+                </details>
+
+            </summary>
+        </details>
+
+<?php require "includes/rodape.php"  ?>
+
+
+    
---
+++ site/includes/planos.php
@@ -0,0 +1,10 @@
+<?php require "includes/cabecalho.php"  ?>
+
+        <h2>Página de Planos</h2>
+        <p>Veja nossos Planos...</p>
+        
+
+<?php require "includes/rodape.php"  ?>
+
+
+    
