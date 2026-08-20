1055778d16a2267e91d75692202483f8f65d6261
lilian2025ju-hash <lilian2025ju@gmail.com>
2026-08-20 09:25:09 -0300

    aplicar títulos personalizados

--- site/includes/cabecalho.php
+++ site/includes/cabecalho.php
@@ -1,12 +1,24 @@
 <?php 
 const BASE = "/site/";
+
+$pagina = basename($_SERVER['PHP_SELF']);
+
+switch($pagina){
+    case 'index.php': $titulo = "Página Inicial"; break;
+    case 'cursos.php':$titulo = "Cusos"; break;
+     case 'duvidas.php':$titulo = "Dúvidas"; break;
+      case 'planos.php':$titulo = "Planos"; break;
+      default: $titulo =  "Consultoria"; break;
+}
 ?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usando PHP</title>
+    <title><?= $titulo  ?> Usando PHP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="<?= BASE ?>css/estilos.css">
     
